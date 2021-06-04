ee80bc0c8044136d99b987fb5a5e314946e7550f
Eliud Maina <chiuri@uoemdomain.embuni.ac.ke>
2021-06-04 15:22:45 +0300

    updated mern

--- server/models/index.js
+++ server/models/index.js
@@ -5,11 +5,8 @@ const db = {};
 
 db.mongoose = mongoose;
 
-db.user = require("./User")(mongoose);
-db.role = require("./Role")(mongoose);
 db.reviews = require("./Reviews");
 db.blog = require('./Blog');
 
-db.ROLES = ["user", "admin", "moderator"];
 
 module.exports = db;
--- server/server.js
+++ server/server.js
@@ -19,57 +19,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/auth.routes")(app);
-require("./routes/user.routes")(app);
+
 app.use('/users', UserRoutes);
 app.use('/blog', BlogRoutes);
 
 db.mongoose.connect(process.env.DB, { useNewUrlParser: true })
   .then(() => {
     console.log("Successfully connect to MongoDB.");
-    initial();
+   
   })
   .catch((err) => {
     console.error("Connection error", err);
     process.exit();
   });
 
-
-		function initial() {
-      Role.collection.estimatedDocumentCount((err, count) => {
-        if (!err && count === 0) {
-          new Role({
-            name: "user",
-          }).save((err) => {
-            if (err) {
-              console.log("error", err);
-            }
-
-            console.log("added 'user' to roles collection");
-          });
-
-          new Role({
-            name: "moderator",
-          }).save((err) => {
-            if (err) {
-              console.log("error", err);
-            }
-
-            console.log("added 'moderator' to roles collection");
-          });
-
-          new Role({
-            name: "admin",
-          }).save((err) => {
-            if (err) {
-              console.log("error", err);
-            }
-
-            console.log("added 'admin' to roles collection");
-          });
-        }
-      });
-    }
 app.get('/may', (req,res)=>{
 	res.render('index');
 })
