3e4abe1d1b743acbd941b692411e7177d3c496ca
elithecompguru <eliudmaina064@gmail.com>
2021-06-09 10:15:29 +0300

    figuring heroku error

--- server.js
+++ server.js
@@ -44,9 +44,9 @@ app.get('/may', (req,res)=>{
 
 // for deployment
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static("/client/build"));
+  app.use('/', express.static("/client/build"));
   app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "./client/build","index.html"));
+    response.sendFile(path.resolve(__dirname, "./client/build/index.html"));
   });
 }
 
