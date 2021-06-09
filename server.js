5da9dc776d0e2219b171afa6e9ddcfc7626aedca
elithecompguru <eliudmaina064@gmail.com>
2021-06-09 09:25:18 +0300

    figuring heroku error

--- server.js
+++ server.js
@@ -12,39 +12,37 @@ const BlogRoutes = require('./routes/Blog')
 require("dotenv").config();
 
 const app = express();
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+//to routes
 app.use('/users', UserRoutes);
 app.use('/blog', BlogRoutes);
 
-// Step 1:
-app.use(express.static(path.join(__dirname, "/client/build")));
-// Step 2:
-
+//db connection
 db.mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
     console.log("Successfully connect to MongoDB.");
-   
   })
   .catch((err) => {
     console.error("Connection error", err);
     process.exit();
   });
 
-
+//ejs template
 app.get('/may', (req,res)=>{
 	res.render('index');
 })
 
-
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+// for deployment
+if(process.env.NODE_ENV === 'production'){
+  app.use('/', express.static("/client/build"));
+  app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+  });
+}
 
 app.listen(PORT);
