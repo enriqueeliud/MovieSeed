57bae509fd452d067997c6b55a40bb36995a9520
elithecompguru <eliudmaina064@gmail.com>
2021-06-09 10:38:02 +0300

    figuring heroku error

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get('/may', (req,res)=>{
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../client/build')); // serve the static react app
   app.get(/^\/(?!api).*/, (req, res) => { // don't serve api routes to react app
-    res.sendFile(path.join(__dirname, "../client", "public", "index.html"));
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
   });
   console.log('Serving React App...');
 };
