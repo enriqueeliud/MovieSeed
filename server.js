47be45ae92b03e18c9305cd4c486dda82c5cedc2
elithecompguru <eliudmaina064@gmail.com>
2021-06-09 11:32:28 +0300

    figuring heroku error

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.set("view engine", "ejs");
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "client/build")));
 //to routes
 app.use('/users', UserRoutes);
 app.use('/blog', BlogRoutes);
@@ -39,12 +40,9 @@ app.get('/may', (req,res)=>{
 
 // for deployment
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.resolve("../client/build")));
-  app.get("*", function (request, response) {
-    response.sendFile(
-      path.resolve(__dirname, '../client', 'build', 'index.html')
-    );
-  });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 }
 
 app.listen(PORT);
