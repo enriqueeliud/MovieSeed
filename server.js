4bea56ef2418316973332d58378dbc029e372aa0
elithecompguru <eliudmaina064@gmail.com>
2021-06-09 11:17:54 +0300

    figuring heroku error

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.resolve("../client/build")));
   app.get("*", function (request, response) {
     response.sendFile(
-      path.resolve(__dirname, "../client', 'build', 'index.html")
+      path.resolve(__dirname, '../client', 'build', 'index.html')
     );
   });
 }
